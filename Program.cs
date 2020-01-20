15709c05e51cc38f67e977bc47f7c8ce725f0ea0
Timur Ercan <timur.ercan31@gmail.com>
2020-01-20 07:56:24 +0100

    trigger times

--- Program.cs
+++ Program.cs
@@ -4,9 +4,68 @@ namespace backgroundr
 {
     class Program
     {
+        private static int _updateIntervall = 60000;
+        private static string imageFolder = "";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                checkBackground();
+                System.Threading.Thread.Sleep(_updateIntervall);
+            }
+        }
+
+        private static void checkBackground()
+        {
+            var now = DateTime.Now;
+            var backgroundImage = "";
+
+            if (IsNight())
+            {
+                backgroundImage = "1.jpg";
+            }
+
+            if (IsMorning())
+            {
+                backgroundImage = "2.jpg";
+            }
+
+            if (IsDay())
+            {
+                backgroundImage = "3.jpg";
+            }
+
+            if (IsEvening())
+            {
+                backgroundImage = "4.jpg";
+            }
+
+            setBackground(backgroundImage);
+        }
+
+        private static bool IsNight()
+        {
+            return DateTime.Now.Hour < 6;
+        }
+
+        private static bool IsMorning()
+        {
+            return DateTime.Now.Hour > 6 && DateTime.Now.Hour < 9;
+        }
+
+        private static bool IsDay()
+        {
+            return DateTime.Now.Hour > 9 && DateTime.Now.Hour < 18;
+        }
+
+        private static bool IsEvening()
+        {
+            return DateTime.Now.Hour >= 18;
+        }
+
+        private static void setBackground(string backgroundImage)
+        {
+
         }
     }
 }
